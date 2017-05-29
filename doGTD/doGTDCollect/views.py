8169952b93c320f288427006ba6bc3a136618f48
danowenuk <danowen@gmail.com>
2017-05-29 20:53:04 +1000

    part 5 done

--- doGTD/doGTDCollect/views.py
+++ doGTD/doGTDCollect/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index2(request):
-    return HttpResponse("<h2>doGTD Collect App placeholder</h2>")
+def simple(request):
+    return HttpResponse("<h2>A simple HTML page...</h2>")
 
 def index(request):
     return render(request, 'doGTDCollect/home.html')
+
+def workflow(request):
+    return render(request, 'doGTDCollect/workflow.html')
+
+def contact(request):
+    return render(request, 'doGTDCollect/basic.html',{'content':['If you would like to contact me, please email me.','danowen@gmail.com']})
